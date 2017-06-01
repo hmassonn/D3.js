d4d8de0c3a37db21d7239006c5ad3abb53595d8d
hmassonn <hmassonn@student42.fr>
2017-06-01 15:17:30 +0200

    new pie chart

--- jcdecaux/src/piechart.js
+++ jcdecaux/src/piechart.js
@@ -1,16 +1,16 @@
 (function(d3) {
     'use strict';
 
-    var width = 360;
-    var height = 360;
-    var radius = Math.min(width, height) / 2;
-    var donutWidth = 75;
-    var legendRectSize = 18;
-    var legendSpacing = 4;
+    let width = 360;
+    let height = 360;
+    let radius = Math.min(width, height) / 2;
+    let donutWidth = 75;
+    let legendRectSize = 18;
+    let legendSpacing = 4;
 
-	var color = d3.scaleOrdinal(d3.schemeCategory20);
+	let color = d3.scaleOrdinal(d3.schemeCategory20);
 
-    var svg = d3.select('#chart')
+    let svg = d3.select('#chart')
       .append('svg')
       .attr('width', width)
       .attr('height', height)
@@ -18,15 +18,15 @@
       .attr('transform', 'translate(' + (width / 2) +
         ',' + (height / 2) + ')');
 
-    var arc = d3.arc()
+    let arc = d3.arc()
       .innerRadius(radius - donutWidth)
       .outerRadius(radius);
 
-    var pie = d3.pie()
+    let pie = d3.pie()
       .value(function(d) { return d.count; })
       .sort(null);
 
-    var tooltip = d3.select('#chart')
+    let tooltip = d3.select('#chart')
       .append('div')
       .attr('class', 'tooltip');
 
@@ -46,7 +46,7 @@
         d.enabled = true;
       });
 
-      var path = svg.selectAll('path')
+      let path = svg.selectAll('path')
         .data(pie(dataset))
         .enter()
         .append('path')
@@ -57,11 +57,10 @@
         .each(function(d) { this._current = d; });
 
       path.on('mouseover', function(d) {
-        var total = d3.sum(dataset.map(function(d) {
+        let total = d3.sum(dataset.map(function(d) {
           return (d.enabled) ? d.count : 0;
         }));
-
-        var percent = Math.round(1000 * d.data.count / total) / 10;
+        let percent = Math.round(1000 * d.data.count / total) / 10;
         tooltip.select('.label').html(d.data.label);
         tooltip.select('.count').html(d.data.count);
         tooltip.select('.percent').html(percent + '%');
@@ -77,7 +76,7 @@
           .style('left', (d3.event.layerX + 10) + 'px');
       });
 
-      var legend = svg.selectAll('.legende')
+      let legend = svg.selectAll('.legende')
         .data(color.domain())
         .enter()
         .append('g')
@@ -86,10 +85,10 @@
 			if (i <= 4) {
 				return 'translate(' + (-2000) + ',' + (-2000) + ')';
 			}
-	          var height = legendRectSize + legendSpacing;
-	          var offset =  height * color.domain().length / 2;
-	          var horz = -2 * legendRectSize;
-	          var vert = (i-3) * height - offset;
+	          let height = legendRectSize + legendSpacing;
+	          let offset =  height * color.domain().length / 2;
+	          let horz = -2 * legendRectSize;
+	          let vert = (i-3) * height - offset;
           return 'translate(' + horz + ',' + vert + ')';
         });
 
@@ -99,31 +98,29 @@
         .style('fill', color)
         .style('stroke', color)
         .on('click', function(label) {
-          var rect = d3.select(this);
-          var enabled = true;
-          var totalEnabled = d3.sum(dataset.map(function(d) {
+          let rect = d3.select(this);
+          let enabled = true;
+          let totalEnabled = d3.sum(dataset.map(function(d) {
             return (d.enabled) ? 1 : 0;
           }));
-
           if (rect.attr('class') === 'disabled') {
             rect.attr('class', '');
           } else {
-            if (totalEnabled < 2) return;
+            if (totalEnabled < 2)
+				return ;
             rect.attr('class', 'disabled');
             enabled = false;
           }
-
           pie.value(function(d) {
-            if (d.label === label) d.enabled = enabled;
+            if (d.label === label)
+				d.enabled = enabled;
             return (d.enabled) ? d.count : 0;
           });
-
           path = path.data(pie(dataset));
-
           path.transition()
             .duration(750)
             .attrTween('d', function(d) {
-              var interpolate = d3.interpolate(this._current, d);
+              let interpolate = d3.interpolate(this._current, d);
               this._current = interpolate(0);
               return function(t) {
                 return arc(interpolate(t));
@@ -140,52 +137,80 @@
 
 	// second pie chart
 	// http://www.spiria.com/fr/blogue/culture-geek/programmation-les-tendances-2016
-		var dataset = [
-		  { label: 'OS X', count: 26.2 },
-		  { label: 'Windows 7', count: 22.5 },
-		  { label: 'Linux', count: 21.7 },
-		  { label: 'Windows 10', count: 20.8 },
-		  { label: 'Windows 8', count: 8.4 }
+		let datas = [
+		  { label: 'OS X', count: 26.2, enabled: true},
+		  { label: 'Windows 7', count: 22.5, enabled: true},
+		  { label: 'Linux', count: 21.7, enabled: true},
+		  { label: 'Windows 10', count: 20.8, enabled: true},
+		  { label: 'Windows 8', count: 8.4, enabled: true}
 		];
 
-		var svg2 = d3.select('#pie')
+		let svg2 = d3.select('#pie')
 			.append('svg')
 			.attr('width', width*2)
 			.attr('height', height)
 			.append('g')
 			.attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
 
-		var arc2 = d3.arc()
+		let arc2 = d3.arc()
 			.innerRadius(0)
 	      	.outerRadius(radius);
 
-		var path2 = svg2.selectAll('path')
-			.data(pie(dataset))
+		let path2 = svg2.selectAll('path')
+			.data(pie(datas))
 			.enter()
 			.append('path')
 			.attr('d', arc2)
 			.attr('fill', function(d, i) {
 				return color(d.data.label);
-		  });
+		  	});
 
-		var legend2 = svg2.selectAll('.legend')
+		let legend2 = svg2.selectAll('.legend')
 			.data(color.domain())
 			.enter()
 			.append('g')
 			.attr('class', 'legend')
 			.attr('transform', function(d, i) {
-			var h = legendRectSize + legendSpacing;
-			var offset =  h * color.domain().length / 2;
-			var horz = -2 * legendRectSize;
-			var vert = i * h - offset;
-				return 'translate(' + (width/2+legendSpacing) + ',' + (vert+h*dataset.length-(height/2)) + ')';
+				let h = legendRectSize + legendSpacing;
+				let offset =  h * color.domain().length / 2;
+				let horz = -2 * legendRectSize;
+				let vert = i * h - offset;
+					return 'translate(' + (width/2+legendSpacing) + ',' + (vert+h*datas.length-(height/2)) + ')';
 			});
 
 		legend2.append('rect')
 	          .attr('width', legendRectSize)
 	          .attr('height', legendRectSize)
 	          .style('fill', color)
-	          .style('stroke', color);
+	          .style('stroke', color)
+			  .on('click', function(label) {
+	  	          let rect = d3.select(this);
+	  	          let enabled = true;
+	  	          d3.sum(datas.map(function(d) {
+	  	            return (d.enabled) ? 1 : 0;
+	  	          }));
+	  	          if (rect.attr('class') === 'disabled') {
+	  	            rect.attr('class', '');
+	  	          } else {
+	  	            rect.attr('class', 'disabled');
+	  	            enabled = false;
+	  	          }
+	  	          pie.value(function(d) {
+	  	            if (d.label === label) {
+	  					d.enabled = enabled;
+					}
+	  	            return (d.enabled) ? d.count : 0;
+	  	          });
+	  	          path2 = path2.data(pie(datas));
+	  	          path2.transition()
+	  	            .attrTween('d', function(d) {
+	  	              let interpol = d3.interpolate(this._current, d);
+	  	              this._current = interpol(0);
+	  	              return function(t) {
+	  	                return arc2(interpol(t));
+	  	              };
+	  	            });
+	  	        });
 
 	    legend2.append('text')
 	      .attr('x', legendRectSize + legendSpacing)
--- jcdecaux/src/script.js
+++ jcdecaux/src/script.js
@@ -1,27 +1,29 @@
 d3.json('forecast.json', function(d) {
 
-	  var temperatures = [],
-		      dates = [],
-		      margin = { top: 0, right: 0, bottom: 30, left: 20 }
+	  let temperatures = [],
+		  dates = [],
+		  humidity = [],
+		  margin = { top: 0, right: 0, bottom: 30, left: 20 },
 	      height = 400 - margin.top - margin.bottom,
-		      width = 600 - margin.left - margin.right;
+		  width = 600 - margin.left - margin.right;
 
-	  var   tempColor,
-		        yScale,
-		        yAxisValues,
-		        yAxisTicks,
-		        yGuide,
-		        xScale,
-		        xAxisValues,
-		        xAxisTicks,
-		        xGuide,
-		        colors,
-		        tooltip,
-		        myChart;
+	  let tempColor,
+		  yScale,
+		  yAxisValues,
+		  yAxisTicks,
+		  yGuide,
+		  xScale,
+		  xAxisValues,
+		  xAxisTicks,
+		  xGuide,
+		  colors,
+		  tooltip,
+		  myChart;
 
-	  for (var i = 0; i<d.list.length; i++) {
+	  for (let i = 0; i < d.list.length; i++) {
 		      temperatures.push(d.list[i].main.temp);
 		      dates.push( new Date(d.list[i].dt_txt) );
+			  humidity.push(d.list[i].main.humidity);
 		    }
 
 	  yScale = d3.scaleLinear()
@@ -63,39 +65,37 @@ d3.json('forecast.json', function(d) {
 	    .attr('width', width + margin.left + margin.right)
 	    .attr('height', height + margin.top + margin.bottom)
 	    .append('g')
-	    .attr('transform',
-			      'translate(' + margin.left + ',' + margin.right + ')')
+	    .attr('transform', 'translate(' + margin.left + ',' + margin.right + ')')
 	    .selectAll('rect').data(temperatures)
 	    .enter().append('rect')
-	      .attr('fill', colors)
-	      .attr('width', function(d) {
-			          return xScale.bandwidth();
-			        })
-	      .attr('height', 0)
-	      .attr('x', function(d) {
-			          return xScale(d);
-			        })
-	      .attr('y', height)
-
-	      .on('mouseover', function(d) {
-			          tooltip.transition().duration(200)
-			            .style('opacity', .9)
-			          tooltip.html(
-						            '<div style="font-size: 2rem; font-weight: bold">' +
-							              d + '&deg;</div>'
-						          )
-			            .style('left', (d3.event.pageX -35) + 'px')
-			            .style('top', (d3.event.pageY -30) + 'px')
-			          tempColor = this.style.fill;
-			          d3.select(this)
-			            .style('fill', 'yellow')
-			        })
-
-	      .on('mouseout', function(d) {
-			          tooltip.html('')
-			          d3.select(this)
-			            .style('fill', tempColor)
-			        });
+		.attr('fill', colors)
+		.attr('width', function(d) {
+		          return xScale.bandwidth();
+		        })
+		.attr('height', 0)
+		.attr('x', function(d) {
+		          return xScale(d);
+		        })
+		.attr('y', height)
+		.on('mouseover', function(d) {
+          tooltip.transition()
+			.duration(200)
+        	.style('opacity', .9)
+          tooltip.html(
+			            '<div style="font-size: 2rem; font-weight: bold">' +
+				              d + '&deg;</div>'
+			          )
+            .style('left', (d3.event.pageX -35) + 'px')
+            .style('top', (d3.event.pageY -30) + 'px')
+          tempColor = this.style.fill;
+          d3.select(this)
+            .style('fill', 'yellow')
+        })
+		.on('mouseout', function(d) {
+		  tooltip.html('')
+		  d3.select(this)
+		    .style('fill', tempColor)
+		});
 
 	  yGuide = d3.select('#viz svg').append('g')
 	            .attr('transform', 'translate(20,0)')
@@ -118,4 +118,91 @@ d3.json('forecast.json', function(d) {
 	    .duration(1000)
 	    .ease(d3.easeBounceOut)
 
-}); // json import
+		// second histograms, TO DO probleme de nb de rectangle (32>40)? et axes a ajouter
+
+	let yS = d3.scaleLinear()
+	  	    	.domain([0, d3.max(humidity)])
+	  	    	.range([0,height]);
+
+	let xS = d3.scaleBand()
+			  	.domain(humidity)
+			  	.paddingInner(.1)
+			  	.paddingOuter(.1)
+			  	.range([0, width])
+
+	let yAV = d3.scaleLinear()
+			  .domain([0, d3.max(humidity)])
+			  .range([height,0]);
+
+	let yAT = d3.axisLeft(yAV)
+				.ticks(10)
+
+	let xAV = d3.scaleTime()
+			  .domain([dates[0],dates[(dates.length-1)]])
+			  .range([0, width])
+
+	let xAT = d3.axisBottom(xAV)
+	  			.ticks(d3.timeDay.every(1))
+
+	let color = d3.scaleLinear()
+			  		.domain([0, d3.max(humidity)/2, d3.max(humidity)])
+			  		.range(['#FFFFFF', '#aaf9ff', '#00035b'])
+
+	let Chart = d3.select('#viz2')
+					.append('svg')
+					.attr('width', width + margin.left + margin.right)
+					.attr('height', height + margin.top + margin.bottom)
+					.append('g')
+					.attr('transform', 'translate(' + margin.left + ',' + margin.right + ')')
+					.selectAll('rect').data(humidity)
+					.enter().append('rect')
+					.attr('fill', color)
+					.attr('width', function(d) {
+					          return xS.bandwidth();
+					        })
+					.attr('height', 0)
+					.attr('x', function(d) {
+					          return xS(d);
+					        })
+					.attr('y', height)
+					.on('mouseover', function(d) {
+			          tooltip.transition()
+						.duration(200)
+			        	.style('opacity', .9)
+			          tooltip.html(
+						            '<div style="font-size: 2rem; font-weight: bold">' +
+							              d + '%</div>'
+						          )
+			            .style('left', (d3.event.pageX -35) + 'px')
+			            .style('top', (d3.event.pageY -30) + 'px')
+			          tempColor = this.style.fill;
+			          d3.select(this)
+			            .style('fill', 'white')
+			        })
+					.on('mouseout', function(d) {
+					  tooltip.html('')
+					  d3.select(this)
+					    .style('fill', tempColor)
+					});
+
+	yGuide = d3.select('#viz2 svg').append('g')
+   		   .attr('transform', 'translate(20,0)')
+   		   .call(yAT)
+
+    xGuide = d3.select('#viz2 svg').append('g')
+   		   .attr('transform', 'translate(20,'+ height + ')')
+   		   .call(xAT)
+
+	Chart.transition()
+		    .attr('height', function(d) {
+				      return yS(d);
+				    })
+			.attr('y', function(d) {
+				      return height - yS(d);
+				    })
+			.delay(function(d, i) {
+				      return i * 20;
+				    })
+		    .duration(1000)
+
+});
